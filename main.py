a75351f4031e5ee6a6c4e9d6dae08ef28ea96291
wu546300070 <42727019+wu546300070@users.noreply.github.com>
2022-01-14 21:35:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,4 +14,5 @@ if __name__ == '__main__':
     print_hi('PyCharm')
     print_hi("hhhhhhhhhhhhhhhhhhhhhhhhhhh")
     print_hi("wwwwwwwwwwwwwwwwwwwwwwww")
+    print_hi("这是第三处更改")
 # 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
