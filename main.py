12ca17e63826556a24d3abcc578f10926b1ecfcb
wu546300070 <42727019+wu546300070@users.noreply.github.com>
2022-01-14 22:19:10 +0800

    更新

--- main.py
+++ main.py
@@ -16,4 +16,8 @@ if __name__ == '__main__':
     print_hi("wwwwwwwwwwwwwwwwwwwwwwww")
     print_hi("这是第三处更改")
     print("444444444444444")
+    print("444444444444444")
+    print("444444444444444")
+    print("444444444444444")
+    print("444444444444444")
 # 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
