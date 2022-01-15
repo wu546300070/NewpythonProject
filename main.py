65825cd6c4297bfdb41f6a3169cc7fe1629b4434
wu546300070 <42727019+wu546300070@users.noreply.github.com>
2022-01-15 12:04:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,4 +19,6 @@ if __name__ == '__main__':
     print_hi("这是第三处更改")
     print_hi("99999999999999999999999999999999")
     print_hi("99999999999999999999999999999999")
+    print_hi("xxxxxxxxxxxxxxxxxxx")
+    print_hi("yyyyyyyyyyyyyyyyyyyy")
 # 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
